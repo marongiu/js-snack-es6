da9b1c934cbf96b869729dca67dfa49f981700ef
Andrea <andreamarongiu05@gmail.com>
2021-03-18 18:14:55 +0100

    Creo le squadre

--- 2/main.js
+++ 2/main.js
@@ -1,1 +1,43 @@
-console.log("ok");
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi
+// contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+$(document).ready(function() {
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+ const squadre = [
+   {
+     nome: "Juventus",
+     punti: 0,
+     falli: 0
+   },
+   {
+     nome: "Milan",
+     punti: 0,
+     falli: 0
+   },
+   {
+     nome: "Roma",
+     punti: 0,
+     falli: 0
+   }
+ ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // fine ready
