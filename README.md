25f70bf5e2c15aae713297be4f292dee75ec78c9
Andrea <andreamarongiu05@gmail.com>
2021-03-19 16:24:17 +0100

    Utilizzando operatore spread unisco le due array

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-js-snack-es6
+Esercizi JavaScript
---
+++ cats/milestone3/main.js
@@ -0,0 +1,51 @@
+// Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome e colore e opacità del fiocco per ogni gatto.
+$(document).ready(function() {
+  // ricopio l'array del precedente esercizio
+  const gatti = [
+    {
+      nome: "Romeo",
+      eta: 2,
+      colore: "nero",
+      sesso: "M",
+    },
+    {
+      nome: "Luna",
+      eta: 1,
+      colore: "bianco",
+      sesso: "F",
+    },
+    {
+      nome: "Leo",
+      eta: 3,
+      colore: "marrone",
+      sesso: "M",
+    },
+    {
+      nome: "Briciola",
+      eta: 5,
+      colore: "panna",
+      sesso: "F",
+    },
+    {
+      nome: "Virgo",
+      eta: 4,
+      colore: "panna",
+      sesso: "M",
+    },
+    {
+      nome: "Kikia",
+      eta: 14,
+      colore: "panna",
+      sesso: "F",
+    }
+  ];
+
+// mi ricopio la divisione di maschi e femmine con il filter
+const maschi = gatti.filter((item) => item.sesso == "M");
+const femmine = gatti.filter((item) => item.sesso == "F");
+
+// utilizzando  l'operatore Spread unisco le due array
+let gattiUniti = [...femmine,...maschi];
+console.log(gattiUniti);
+
+}); // fine ready
